2eea23e218412bfa3cda50862af6fe6edbb4f01a
Robert Radford <RobertR928@gmail.com>
2021-05-23 21:47:35 -0400

    initialize git repo

---
+++ pet_supplies_plus_inventory/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.urls import path, re_path
+
+# Import all classes from the .views file
+# from .views import test_map_view
+from . import views
+
+urlpatterns = [
+    path('', views.home_view, name='home'),
+]
