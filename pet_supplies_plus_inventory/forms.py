34d3671229851aa048c5434f1bc304a58bee6419
Robert Radford <RobertR928@gmail.com>
2021-06-15 20:27:20 -0400

    restyled

--- pet_supplies_plus_inventory/forms.py
+++ pet_supplies_plus_inventory/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import stockItem
 
-brands = stockItem.objects.values_list('brand', flat=True).distinct()
+brands = stockItem.objects.values_list('brand', flat=True).order_by('brand').distinct()
 
 brand_choices = [(str(i+1), brand) for i, brand in enumerate(brands)]
 brand_choices.insert(0, ('0', 'All Brands'))
